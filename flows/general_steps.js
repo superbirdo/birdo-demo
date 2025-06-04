a203494108071a0cfd3990f7f270454186fe9e9a
superbirdo <77795027+superbirdo@users.noreply.github.com>
2025-06-04 17:07:37 -0400

    Create general_steps.js
    Created section for general troubleshooting steps

---
+++ flows/general_steps.js
@@ -0,0 +1,20 @@
+export const generalTroubleshootingSteps = [
+  { html: `<ol>
+    <li>Verify that you <a href="https://help.shopify.com/en/manual/sell-in-person/hardware/card-readers/tap-chip/overview" target="_blank">meet the requirements</a> to use the card reader.</li>
+    <li>Verify that you have an internet connection. Shopify POS needs to connect to the internet through Wi-Fi or your device's cellular data to process payments. If you can't connect to the internet, then you need to use a different payment type during the time you're offline.</li>
+    <li>Verify that the card reader is listed as a connected device in the POS app by tapping the connectivity icon in the navigation. If the card reader isn't connected, then <a href="https://help.shopify.com/en/manual/sell-in-person/hardware/card-readers/tap-chip/set-up#pair" target="_blank">pair the card reader</a> with your device.</li>
+    <li>Make sure <a href="https://apps.apple.com/us/app/shopify-pos/id796883893" target="_blank"><b>the Shopify POS app is up to date</b></a>.</li>
+    <li>Make sure that your device is up to date:
+      <ul>
+        <li><b>iOS:</b> Go to <b>Settings &gt; General &gt; Software Update</b>. If there's an update available, <b>tap Update now</b>.</li>
+        <li><b>Android:</b> Go to <b>Settings &gt; Software update</b>. If there's an update available, follow the on-screen instructions to install it.</li>
+      </ul>
+    </li>
+    <li><a href="https://help.shopify.com/en/manual/sell-in-person/shopify-pos/setting-up-your-network-for-shopify-pos#firewall-settings" target="_blank">Check your firewall settings</a>.</li>
+    <li>If the reader requires a software update, and the reader doesn't have at least 50% of its battery charged, then the reader won't connect to Shopify POS. Connect the reader's charger and allow it to charge to at least 50% of its battery before reconnecting the reader to Shopify POS.</li>
+    <li>Verify that your device has <a href="https://help.shopify.com/en/manual/sell-in-person/hardware/card-readers/tap-chip/security-features" target="_blank">location services</a> turned on and that the Shopify POS app has the necessary permissions to access your device's location.</li>
+    <li>Verify that Bluetooth on your Shopify POS device is turned on.</li>
+    <li>Verify that the POS app has permission to connect to Bluetooth in your device settings.</li>
+    <li>Perform a pinhole reset of your reader. The pinhole is located next to the charging port on your reader. Wait for the card reader's status lights to blink 4 times to indicate that the reader is connected.</li>
+  </ol>` }
+];
