fe1a81691f6714fab60e7b7f75f132ccde5d683b
superbirdo <77795027+superbirdo@users.noreply.github.com>
2025-06-04 16:57:05 -0400

    Update main.js
    Updated formatting of general troubleshooting steps

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const generalTroubleshootingSteps = [
     <li>Verify that you <a href="https://help.shopify.com/en/manual/sell-in-person/hardware/card-readers/tap-chip/overview" target="_blank">meet the requirements</a> to use the card reader.</li>
     <li>Verify that you have an internet connection. Shopify POS needs to connect to the internet through Wi-Fi or your device's cellular data to process payments. If you can't connect to the internet, then you need to use a different payment type during the time you're offline.</li>
     <li>Verify that the card reader is listed as a connected device in the POS app by tapping the connectivity icon in the navigation. If the card reader isn't connected, then <a href="https://help.shopify.com/en/manual/sell-in-person/hardware/card-readers/tap-chip/set-up#pair" target="_blank">pair the card reader</a> with your device.</li>
-    <li>Make sure <a href="https://help.shopify.com/en/manual/sell-in-person/shopify-pos/troubleshooting#make-sure-that-your-software-is-up-to-date" target="_blank">the Shopify POS app is up to date</a>.</li>
+    <li>Make sure <a href="https://apps.apple.com/us/app/shopify-pos/id796883893" target="_blank"><b>the Shopify POS app is up to date</b></a>.</li>
     <li>Make sure that your device is up to date:
       <ul>
-        <li><b>iOS:</b> Go to <b>Settings &gt; General &gt; Software Update</b>. If there's an update available, tap Update now.</li>
+        <li><b>iOS:</b> Go to <b>Settings &gt; General &gt; Software Update</b>. If there's an update available, <b>tap Update now</b>.</li>
         <li><b>Android:</b> Go to <b>Settings &gt; Software update</b>. If there's an update available, follow the on-screen instructions to install it.</li>
       </ul>
     </li>
